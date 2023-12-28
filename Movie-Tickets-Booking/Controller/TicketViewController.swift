852ab45185b4c28b4095bf4d66dcc57f984b9030
lus0401 <66708571+lus0401@users.noreply.github.com>
2023-12-28 20:31:46 +0900

    Need fixing table view

--- Movie-Tickets-Booking/Controller/TicketViewController.swift
+++ Movie-Tickets-Booking/Controller/TicketViewController.swift
@@ -1,18 +1,67 @@
 import Foundation
+import UIKit
+
 
 class TicketViewController: UIViewController
 {
+
+    @IBOutlet weak var ticketTableView: UITableView!
+    
+    var selectedMovie: MovieInfo?
+    
+    var movie: [MovieInfo] = [
+        MovieInfo(title: "서울의 봄", time: "10:00", people: 0, price: 7000, movieImage: UIImage.movie01),
+        MovieInfo(title: "서울의 봄", time: "12:00", people: 0, price: 7000, movieImage: UIImage.movie01),
+        MovieInfo(title: "서울의 봄", time: "14:00", people: 0, price: 7000, movieImage: UIImage.movie01),
+        MovieInfo(title: "노량", time: "10:00", people: 0, price: 7000, movieImage: UIImage.movie02),
+        MovieInfo(title: "노량", time: "12:00", people: 0, price: 7000, movieImage: UIImage.movie02),
+        MovieInfo(title: "노량", time: "14:00", people: 0, price: 7000, movieImage: UIImage.movie02),
+        MovieInfo(title: "짱구는 못말려 극장판", time: "10:00", people: 0, price: 7000, movieImage: UIImage.movie03),
+        MovieInfo(title: "짱구는 못말려 극장판", time: "12:00", people: 0, price: 7000, movieImage: UIImage.movie03),
+        MovieInfo(title: "짱구는 못말려 극장판", time: "14:00", people: 0, price: 7000, movieImage: UIImage.movie03)
+        
+    ]
+    
     
     // MARK: - Ticket View Life Cycle
-    
-    @IBOutlet weak var movieView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ticketTableView.delegate = self
+        ticketTableView.dataSource = self
     }
     
-    @IBAction func check(_ sender: Any) {
-        print("movie.title")
-        print("ㅁㅁㅁㅁㅁ")
+    
+    @IBAction func timeButtonTapped(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        
+        //사용자가 선택한 영화정보 업데이트
+        selectedMovie = movie[tag - 1]
+        
+        switch selectedMovie {
+        case let  movieInfo?:
+            print("영화제목: \(movieInfo.title), 예매시간: \(movieInfo.time), 인원수: \(movieInfo.people), 가격: \(movieInfo.price)")
+        case nil:
+            print("No movie selected.")
+        }
+        
     }
+    
+}
+
+extension TicketViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        //cell.textLabel?.text = movie[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movie.count
+    }
+
+    
 }
---
+++ Movie-Tickets-Booking/Model/MovieInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MovieInfo.swift
+//  Movie-Tickets-Booking
+//
+//  Created by t2023-m0051 on 12/28/23.
+//
+
+import Foundation
+import UIKit
+
+struct MovieInfo {
+    var title: String
+    var time: String
+    var people: Int
+    var price: Int
+    var movieImage: UIImage
+}
+
