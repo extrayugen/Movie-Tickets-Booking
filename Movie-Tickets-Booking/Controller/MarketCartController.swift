dd08d390cf110871659f88f6b0cb8e734ebd8341
yeongnote <eongh0640@gmail.com>
2023-12-29 21:11:44 +0900

    [Chore] 콜렉션 뷰, 테이블 뷰 추가

--- Movie-Tickets-Booking/Controller/MarketCartController.swift
+++ Movie-Tickets-Booking/Controller/MarketCartController.swift
@@ -5,32 +5,75 @@ import UIKit
 class MarketCartController: UIViewController {
     
     @IBOutlet weak var marketInfoTableView: UITableView!
-    
     @IBOutlet weak var selectedMarketCollection: UICollectionView!
+    @IBOutlet weak var marketTotalPriceLabel: UILabel!
+    
+    
+    //마켓에서 사용자가 선택한 간식 정보 저장.
+    var selectedItems: [Fooditems] = [
+        Fooditems(name: "즉석구이(몸+다리)", image: UIImage(named: "Snacks/Squid(Whole)")!, price: "10,000원")
+    ]
+    
+    //이미지 배열
+    var images: [UIImage] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedMarketCollection.backgroundColor = .green
+
+        updateTotalPriceLabel()
+        updateImages()
+    }
+    
+    //총 금액 업데이트
+    // 총 금액 업데이트
+    func updateTotalPriceLabel() {
+        let totalPrice = selectedItems.reduce(0) { (result, item) -> Int in
+            let priceValue = (item.price?.replacingOccurrences(of: "원", with: "")).flatMap { Int($0) } ?? 0
+            return result + priceValue
+        }
+        marketTotalPriceLabel.text = "\(totalPrice)원"
+    }
+    
+    //이미지 배열 업데이트
+    func updateImages() {
+        for item in selectedItems {
+            images.append(item.image)
+        }
+    }
+    //결제하기 버튼
+    @IBAction func paymentTapped(_ sender: Any) {
+        //결제 동작 구현
     }
 }
 
 
 extension MarketCartController: UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate {
     
+    //테이블 뷰
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return selectedItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = marketInfoTableView.dequeueReusableCell(withIdentifier: "marketCell", for: indexPath)
+        cell.textLabel?.text = selectedItems[indexPath.row].name
+        return cell
     }
     
+    
+    //콜렉션 뷰
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return images.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let imageView = cell.contentView.subviews.first as? UIImageView {
+            imageView.image = images[indexPath.row]
+        }
+        return cell
     }
     
     
--- Movie-Tickets-Booking/Model/FoodMarketInfor.swift
+++ Movie-Tickets-Booking/Model/FoodMarketInfor.swift
@@ -9,49 +9,49 @@ import UIKit
 struct Fooditems{
     let name : String
     let image : UIImage
-    let price : String
+    let price : String?
 }
 let comboItems: [Fooditems] = [
-    Fooditems(name: "더블콤보", image: UIImage(named: "Combo/DoubleCombo")!, price: "13,000원"),
-    Fooditems(name: "커플콤보", image: UIImage(named: "Combo/CoupleCombo")!, price: "10,000원"),
-    Fooditems(name: "맥주콤보", image: UIImage(named: "Combo/BeerCombo")!, price: "18,500원"),
-    Fooditems(name: "즉석구이콤보", image: UIImage(named: "Combo/SquidCombo")!, price: "20,000원"),
-    Fooditems(name: "코카콜라베어콤보", image: UIImage(named: "Combo/CokeBearCombo")!, price: "15,000원")
+    Fooditems(name: "더블콤보", image: UIImage(named: "DoubleCombo")!, price: "13,000원"),
+    Fooditems(name: "커플콤보", image: UIImage(named: "CoupleCombo")!, price: "10,000원"),
+    Fooditems(name: "맥주콤보", image: UIImage(named: "BeerCombo")!, price: "18,500원"),
+    Fooditems(name: "즉석구이콤보", image: UIImage(named: "SquidCombo")!, price: "20,000원"),
+    Fooditems(name: "코카콜라베어콤보", image: UIImage(named: "CokeBearCombo")!, price: "15,000원")
 ]
 let popcornItems: [Fooditems] = [
-    Fooditems(name: "오리지널팝콘R", image: UIImage(named: "Popcorn/OriginalPopcornR")!, price: "5,000원"),
-    Fooditems(name: "오리지널팝콘L", image: UIImage(named: "Popcorn/OriginalPopcornL")!, price: "5,500원"),
-    Fooditems(name: "갈릭팝콘R", image: UIImage(named: "Popcorn/GarlicPopcornR")!, price: "6,000원"),
-    Fooditems(name: "갈릭팝콘L", image: UIImage(named: "Popcorn/GarlicPopcornL")!, price: "6,500원"),
-    Fooditems(name: "더블카라멜팝콘R", image: UIImage(named: "Popcorn/CaramelPopcornR")!, price: "7,000원"),
-    Fooditems(name: "더블카라멜팝콘L", image: UIImage(named: "Popcorn/CaramelPopcornL")!, price: "7,500원"),
-    Fooditems(name: "오리지널+갈릭팝콘L", image: UIImage(named: "Popcorn/Origial+GarlicPopcornL")!, price: "6,500원"),
+    Fooditems(name: "오리지널팝콘R", image: UIImage(named: "OriginalPopcornR")!, price: "5,000원"),
+    Fooditems(name: "오리지널팝콘L", image: UIImage(named: "OriginalPopcornL")!, price: "5,500원"),
+    Fooditems(name: "갈릭팝콘R", image: UIImage(named: "GarlicPopcornR")!, price: "6,000원"),
+    Fooditems(name: "갈릭팝콘L", image: UIImage(named: "GarlicPopcornL")!, price: "6,500원"),
+    Fooditems(name: "더블카라멜팝콘R", image: UIImage(named: "CaramelPopcornR")!, price: "7,000원"),
+    Fooditems(name: "더블카라멜팝콘L", image: UIImage(named: "CaramelPopcornL")!, price: "7,500원"),
+    Fooditems(name: "오리지널+갈릭팝콘L", image: UIImage(named: "Origial+GarlicPopcornL")!, price: "6,500원"),
     Fooditems(name: "오리지널+더블카라멜팝콘L", image: UIImage(named: "Popcorn/Caramel+OrigialPopcornL")!, price: "7,500원"),
-    Fooditems(name: "갈릭+더블카라멜팝콘L", image: UIImage(named: "Popcorn/Caramel+GarlicPopcornL")!, price: "7,500원")
+    Fooditems(name: "갈릭+더블카라멜팝콘L", image: UIImage(named: "Caramel+GarlicPopcornL")!, price: "7,500원")
 ]
 
 let drinkItems: [Fooditems] = [
-    Fooditems(name: "제휴음료R", image: UIImage(named: "Drinks/BeverageR")!, price: "3,000원"),
-    Fooditems(name: "제휴음료L", image: UIImage(named: "Drinks/BeverageL")!, price: "3,500원"),
-    Fooditems(name: "오렌지에이드", image: UIImage(named: "Drinks/OrangeAde")!, price: "5,500원"),
-    Fooditems(name: "자몽에이드", image: UIImage(named: "Drinks/GrapefruitAde")!, price: "5,500원"),
-    Fooditems(name: "아메리카노", image: UIImage(named: "Drinks/HotAmericano")!, price: "5,000원"),
-    Fooditems(name: "아이스아메리카노", image: UIImage(named: "Drinks/IceAmericano")!, price: "5,500원"),
-    Fooditems(name: "순수(생수)페트", image: UIImage(named: "Drinks/PureBottleWater")!, price: "2,000원")
+    Fooditems(name: "제휴음료R", image: UIImage(named: "BeverageR")!, price: "3,000원"),
+    Fooditems(name: "제휴음료L", image: UIImage(named: "BeverageL")!, price: "3,500원"),
+    Fooditems(name: "오렌지에이드", image: UIImage(named: "OrangeAde")!, price: "5,500원"),
+    Fooditems(name: "자몽에이드", image: UIImage(named: "GrapefruitAde")!, price: "5,500원"),
+    Fooditems(name: "아메리카노", image: UIImage(named: "HotAmericano")!, price: "5,000원"),
+    Fooditems(name: "아이스아메리카노", image: UIImage(named: "IceAmericano")!, price: "5,500원"),
+    Fooditems(name: "순수(생수)페트", image: UIImage(named: "PureBottleWater")!, price: "2,000원")
 ]
 
 let snackItems: [Fooditems] = [
-    Fooditems(name: "오리지널핫도그", image: UIImage(named: "Snacks/OriginalHotdog")!, price: "5,500원"),
-    Fooditems(name: "살사핫도그", image: UIImage(named: "Snacks/SalsaHotdog")!, price: "7,500원"),
-    Fooditems(name: "즉석구이(몸통)", image: UIImage(named: "Snacks/Squid(body)")!, price: "6,500원"),
-    Fooditems(name: "즉석구이(몸+다리)", image: UIImage(named: "Snacks/Squid(Whole)")!, price: "10,000원"),
-    Fooditems(name: "칠리치즈나쵸", image: UIImage(named: "Snacks/ChillCheeseNachos")!, price: "5,500원")
+    Fooditems(name: "오리지널핫도그", image: UIImage(named: "OriginalHotdog")!, price: "5,500원"),
+    Fooditems(name: "살사핫도그", image: UIImage(named: "SalsaHotdog")!, price: "7,500원"),
+    Fooditems(name: "즉석구이(몸통)", image: UIImage(named: "Squid(body)")!, price: "6,500원"),
+    Fooditems(name: "즉석구이(몸+다리)", image: UIImage(named: "Squid(Whole)")!, price: "10,000원"),
+    Fooditems(name: "칠리치즈나쵸", image: UIImage(named: "ChillCheeseNachos")!, price: "5,500원")
 ]
 
 let goodiesItems: [Fooditems] = [
-    Fooditems(name: "3D짱구피규어셋트", image: UIImage(named: "Goodies/3DShinchanFigureSet")!, price: "23,000원"),
-    Fooditems(name: "꾸래핑음료컵", image: UIImage(named: "Goodies/OkeydokeypingCup")!, price: "19,500원"),
-    Fooditems(name: "하츄핑음료컵", image: UIImage(named: "Goodies/HeartspingCup")!, price: "19,500원"),
-    Fooditems(name: "마이크와조스키팝콘버켓", image: UIImage(named: "Goodies/MikeWazowskiBucket")!, price: "32,000원")
+    Fooditems(name: "3D짱구피규어셋트", image: UIImage(named: "3DShinchanFigureSet")!, price: "23,000원"),
+    Fooditems(name: "꾸래핑음료컵", image: UIImage(named: "OkeydokeypingCup")!, price: "19,500원"),
+    Fooditems(name: "하츄핑음료컵", image: UIImage(named: "HeartspingCup")!, price: "19,500원"),
+    Fooditems(name: "마이크와조스키팝콘버켓", image: UIImage(named: "MikeWazowskiBucket")!, price: "32,000원")
 ]
 
