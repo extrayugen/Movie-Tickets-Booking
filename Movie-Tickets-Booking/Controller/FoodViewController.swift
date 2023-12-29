f472d432ec9dd6fcc5b6339c6c0f2b4cbb337f19
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2023-12-30 04:16:14 +0900

    상하단 과 좌우 여백설정

--- Movie-Tickets-Booking/Controller/FoodViewController.swift
+++ Movie-Tickets-Booking/Controller/FoodViewController.swift
@@ -11,73 +11,92 @@ protocol FoodCellDelegate: AnyObject {
 }
 
 class FoodViewController: UIViewController {
+    
     @IBOutlet weak var foodSegmentControl: UISegmentedControl!
     @IBOutlet weak var foodCollectionView: UICollectionView!
     
     var currentItems: [foodItems] = comboItems // 초기값으로 콤보 아이템 설정
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            foodCollectionView.dataSource = self
-            foodCollectionView.delegate = self
-            foodCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
-            
-            // 세그먼트 값이 변경될 때 호출되는 메서드 등록
-            foodSegmentControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
-        }
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           foodCollectionView.dataSource = self
+           foodCollectionView.delegate = self
+           foodCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
 
-        @objc func segmentChanged() {
-            // 세그먼트에 따라 현재 아이템을 업데이트하고 컬렉션 뷰 리로드
-            switch foodSegmentControl.selectedSegmentIndex {
-            case 0:
-                currentItems = comboItems
-            case 1:
-                currentItems = popcornItems
-            case 2:
-                currentItems = drinkItems
-            case 3:
-                currentItems = snackItems
-            case 4:
-                currentItems = goodiesItems
-            default:
-                break
-            }
-            foodCollectionView.reloadData()
-        }
+           // 세그먼트 값이 변경될 때 호출되는 메서드 등록
+           foodSegmentControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+       }
+
+       @objc func segmentChanged() {
+           // 세그먼트에 따라 현재 아이템을 업데이트하고 컬렉션 뷰 리로드
+           switch foodSegmentControl.selectedSegmentIndex {
+           case 0:
+               currentItems = comboItems
+           case 1:
+               currentItems = popcornItems
+           case 2:
+               currentItems = drinkItems
+           case 3:
+               currentItems = snackItems
+           case 4:
+               currentItems = goodiesItems
+           default:
+               break
+           }
+           foodCollectionView.reloadData()
+       }
+   }
+
+   extension FoodViewController: UICollectionViewDataSource {
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return currentItems.count
+       }
+
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = foodCollectionView.dequeueReusableCell(withReuseIdentifier: "FoodCollectionViewCell", for: indexPath) as! FoodCollectionViewCell
+           cell.delegate = self
+           cell.setup(with: currentItems[indexPath.row])
+           return cell
+       }
+   }
+
+extension FoodViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
 
-    extension FoodViewController: UICollectionViewDataSource {
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return currentItems.count
-        }
-
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = foodCollectionView.dequeueReusableCell(withReuseIdentifier: "FoodCollectionViewCell", for: indexPath) as! FoodCollectionViewCell
-            cell.delegate = self
-            cell.setup(with: currentItems[indexPath.row])
-            return cell
-        }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 
-    extension FoodViewController: UICollectionViewDelegateFlowLayout, FoodCellDelegate {
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            return CGSize(width: 175, height: 220)
-        }
-
-        func foodCellDidTap(food: foodItems) {
-            let alertController = UIAlertController(title: "장바구니에 추가", message: "장바구니에 \(food.name)을(를) 추가하시겠습니까?", preferredStyle: .alert)
-
-            let addAction = UIAlertAction(title: "추가", style: .default) { _ in
-                // 여기에 장바구니에 추가하는 로직을 추가하세요.
-                // 예시: ShoppingCartController에 메서드를 구현하고 호출
-                // ShoppingCartController.addToCart(food: food)
-            }
-
-            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-
-            alertController.addAction(addAction)
-            alertController.addAction(cancelAction)
-
-            present(alertController, animated: true, completion: nil)
-        }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = 10 * 3
+        let availableWidth = collectionView.frame.width - CGFloat(paddingSpace)
+        let widthPerItem = availableWidth / 2
+        return CGSize(width: widthPerItem, height: 220)
+    }
+}
+
+
+   extension FoodViewController: FoodCellDelegate {
+       func foodCellDidTap(food: foodItems) {
+           let alertController = UIAlertController(title: "장바구니에 추가", message: "장바구니에 \(food.name)을(를) 추가하시겠습니까?", preferredStyle: .alert)
+
+           let addAction = UIAlertAction(title: "추가", style: .default) { _ in
+               // 여기에 장바구니에 추가하는 로직을 추가하세요.
+               // 예시: ShoppingCartController에 메서드를 구현하고 호출
+               // ShoppingCartController.addToCart(food: food)
+           }
+
+           let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+           alertController.addAction(addAction)
+           alertController.addAction(cancelAction)
+
+           present(alertController, animated: true, completion: nil)
+       }
+   }
